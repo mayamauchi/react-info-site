0144a818fee4ec0f5feafcbe8319a16d78e7001d
root <root@DESKTOP-MSPGAUN.localdomain>
2022-10-16 18:52:13 -0500

    large image

--- src/index.js
+++ src/index.js
@@ -1,19 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Header from "./Header";
-import Footer from "./Footer";
-import MainContent from "./MainContent";
+import App from "./App";
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <MainContent />
-      <Footer />
-    </div>
-  );
-}
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
